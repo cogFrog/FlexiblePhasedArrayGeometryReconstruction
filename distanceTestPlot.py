4f25fa018c80f40350f1676cbfd5b2404f3634c3
cogFrog <skyebedard@gmail.com>
2022-03-22 11:48:04 -0600

    Added HFSS results, plotted results for distance test.

---
+++ distanceTestPlot.py
@@ -0,0 +1,37 @@
+import numpy as np
+import rangeFinder as rf
+import matplotlib.pyplot as plt
+
+csv = rf.RangeFinderCSV(10e9, 100e6, 101)
+
+# calibrate at 100 mm or 0.1 m
+csv.find_range(True, 0.1, f'results\\distanceResults\\dist_100mm.csv')
+
+actual_dist = np.array([*range(10, 300, 5)])
+estimated_dist = []
+estimated_dist_fsk = []
+
+for dist in range(10, 300, 5):
+    estimated_dist.append(csv.find_range(False, 0, f'results\\distanceResults\\dist_{dist}mm.csv')[2]*1000)
+estimated_dist = np.array(estimated_dist)
+
+for dist in range(10, 300, 5):
+    estimated_dist_fsk.append(csv.find_range(False, 0, f'results\\distanceResults\\dist_{dist}mm.csv')[0]*1000)
+estimated_dist_fsk = np.array(estimated_dist_fsk)
+
+error_dist = estimated_dist - actual_dist
+error_dist_fsk = estimated_dist_fsk - actual_dist
+
+plt.figure(1)
+plt.plot(actual_dist, error_dist)
+plt.xlabel('Actual Distance (mm)')
+plt.ylabel('Distance Estimation Error (mm)')
+
+plt.figure(2)
+plt.plot(actual_dist, error_dist_fsk)
+plt.axhline(y=15, color="black", linestyle="--")
+plt.axhline(y=-15, color="black", linestyle="--")
+plt.xlabel('Actual Distance (mm)')
+plt.ylabel('Distance Estimation Error (mm)')
+
+plt.show()
---
+++ results/distanceResults/csvSplitter.py
@@ -0,0 +1,33 @@
+import csv
+import numpy as np
+
+# For a parametric sweep in HFSS, a single CSV is made for all antenna distances. This splits it into a CSV for each
+# distance, allowing for RangeFinderCSV to parse them one at a time.
+
+freq = {}
+phase = {}
+
+with open('dist_s12.csv') as csvFile:
+    file = csv.reader(csvFile, delimiter=',')
+    next(file, None)  # skip header
+    for row in file:
+        try:
+            if row[0] in freq:
+                freq[row[0]].append(float(row[1]))
+                phase[row[0]].append(float(row[2]))
+            else:
+                freq[row[0]] = [float(row[1])]
+                phase[row[0]] = [float(row[2])]
+        except ValueError:
+            print("")
+
+distances = freq.keys()
+for d in distances:
+    f = np.array(freq[d])
+    p = np.array(phase[d])
+
+    data = np.concatenate(([f], [p]), axis=0).T
+    with open(f'dist_{d}mm.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Frequency (GHz)', 'Phase (deg)'])
+        csvwriter.writerows(data)
